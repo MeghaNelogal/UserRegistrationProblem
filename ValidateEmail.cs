11303ea5185249371b5ae84b8ded5c1e24bea337
Your Name <you@example.com>
2022-08-16 23:58:13 +0530

    [Megha] Add: Email Is Validated

---
+++ ValidateEmail.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProblem
+{
+    public class ValidateEmail
+    {
+        public static string REGEX_EMail = "^(abc).?[a-z]{3,}[@](bl).?(co).?[a-z]{2,}$"; //abc.xyz@bl.co.in
+
+        public bool ValidateEMail(string email)
+        {
+            return Regex.IsMatch(email, REGEX_EMail);
+        }
+    }
+}
+
