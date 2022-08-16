0383ac58663639a771f2f2047ac29f60803df987
Your Name <you@example.com>
2022-08-17 00:07:43 +0530

    [Megha] Add: Predefined Mobile Format

---
+++ NumberFormat.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProblem
+{
+    public class NumberFormat
+    {
+        
+       public static string REGEX_MobileNo = "^[0-9]{1,3}[ ][6789][0-9]{9}$"; // 91 9008077654
+
+         public bool ValidateMobileNo(string MobileNo)
+         {
+                return Regex.IsMatch(MobileNo, REGEX_MobileNo);
+         }
+        
+    }
+}
+
