fc8b28d6275c9e5cd0dcb95394c3cfa4fd606b12
Your Name <you@example.com>
2022-08-16 23:51:33 +0530

    [Megha] Add: Added UC2

--- Program.cs
+++ Program.cs
@@ -8,10 +8,16 @@ namespace RegExDemo
         {
             Console.WriteLine("Welcome To Registration Problem");
             //UC1
-            FirstName firstName = new FirstName();
-            Console.WriteLine("Please Enter your first name");
-            var check = firstName.ValidateFirstName(Console.ReadLine());
-            Console.WriteLine(check);
+            //FirstName firstName = new FirstName();
+            //Console.WriteLine("Please Enter your first name");
+            //var check = firstName.ValidateFirstName(Console.ReadLine());
+            //Console.WriteLine(check);
+
+            //UC2
+            LastName lastName = new LastName();
+            Console.WriteLine("Please Enter your last name");
+            var check2 = lastName.ValidateFirstName(Console.ReadLine());
+            Console.WriteLine(check2);
         }
     }
 }
