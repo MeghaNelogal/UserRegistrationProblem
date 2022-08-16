a24fa6f4adebe938371e36095bf11f297d094afd
Your Name <you@example.com>
2022-08-17 00:28:05 +0530

    [Megha] Add: Password Contains Atleast one numeric

---
+++ Password3.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProblem
+{
+    public class Password3
+    {
+        public static string REGEX_Password3 = "^[A-Z]{1}[A-Z a-z]{7,}[0-9]{1,}$"; //Meghanel7
+
+        public bool ValidatePassword3(string password)
+        {
+            return Regex.IsMatch(password, REGEX_Password3);
+        }
+    }
+}
