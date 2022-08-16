a104de18d988e74e46520ea26f6d48d0eb890224
Your Name <you@example.com>
2022-08-16 23:16:25 +0530

    [Megha] Add: Added UC1

--- Program.cs
+++ Program.cs
@@ -1,10 +1,17 @@
-﻿namespace RegExDemo
+﻿using UserRegistrationProblem;
+
+namespace RegExDemo
 {
     class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To Registration Problem");          
+            Console.WriteLine("Welcome To Registration Problem");
+            //UC1
+            FirstName firstName = new FirstName();
+            Console.WriteLine("Please Enter your first name");
+            var check = firstName.ValidateFirstName(Console.ReadLine());
+            Console.WriteLine(check);
         }
     }
 }
