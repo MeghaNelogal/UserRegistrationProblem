b8a0edb66bbc51ae462749145d7f0f69b11111ac
Your Name <you@example.com>
2022-08-17 00:14:00 +0530

    [Megha] Add: Password Contains min 8 char

---
+++ Password1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProblem
+{
+    public class Password1
+    {
+        public static string REGEX_Password1 = "^[A-Z a-z]{8,}$"; //Meghanelogal
+
+        public bool ValidatePassword1(string password)
+        {
+            return Regex.IsMatch(password, REGEX_Password1);
+        }
+    }
+}
+
